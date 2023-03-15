5df05836daf9965443ef9d8369ae900375457093
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-03-15 18:49:16 -0300

    preparacion del asado

---
+++ 1.ts
@@ -0,0 +1,6 @@
+console.log ("compro lo que preparare");
+console.log ("limpio el lugar de lo que preparare");
+console.log ("pongo el fuego para el asado");
+console.log ("salo la carne en la fuente");
+console.log ("pongo el asado en la parrilla");
+console.log ("prendo fuego y espero a que se haga el asado");
